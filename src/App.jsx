65c23058a54755fef50e86e2de998ff02b4837b3
Kalyanibonde <kalyanibonde05@gmail.com>
2025-03-29 11:59:51 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import AboutUs from "./pages/AboutUs";
 import Merchandise from "./pages/Merchandise";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
