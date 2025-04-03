0cf27a24eca1980639a0b410fd705dc72ff2c859
Kalyanibonde <kalyanibonde05@gmail.com>
2025-04-03 14:55:17 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 import Home from "./pages/Home";
 import Offerings from "./pages/Offerings";
 import GetInvolved from "./pages/GetInvolved";
@@ -7,12 +10,21 @@ import AboutUs from "./pages/AboutUs";
 import Merchandise from "./pages/Merchandise";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import LoginPage from "./pages/Login"; // Replace with actual login page component
+import LoginPage from "./pages/Login";
 import SignInPage from "./pages/Signin";
 import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration in ms
+      once: true, // Ensures animation occurs only once
+    });
+  }, []);
+
   return (
     <Router>
+      <NavBar /> {/* Keep Navbar outside Routes so it's always visible */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Offerings" element={<Offerings />} />
@@ -20,15 +32,12 @@ function App() {
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/Merchandise" element={<Merchandise />} />
         <Route path="/Login" element={<LoginPage />} />
-          <Route path="/Signin" element={<SignInPage />} />
-          <Route path="/ForgotPassword" element={<ForgotPassword />} />
-
-        <Route path="/NavBar" element={<NavBar />} />
-        <Route path="/Footer" element={<Footer />} />
+        <Route path="/Signin" element={<SignInPage />} />
+        <Route path="/ForgotPassword" element={<ForgotPassword />} />
       </Routes>
+      <Footer /> {/* Keep Footer outside Routes so it's always visible */}
     </Router>
   );
 }
 
 export default App;
-
